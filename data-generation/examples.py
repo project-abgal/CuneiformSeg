35b8d8cd32793e18ba15eb0e193c14318456d0a3
woody <piequalsabout314159@gmail.com>
2019-03-07 13:41:13 -0500

    iterative dilation completed

--- data-generation/examples.py
+++ data-generation/examples.py
@@ -14,7 +14,7 @@ def subtract_char(background, patch, x, y):
 if __name__ == "__main__":
     characters = [cv2.cvtColor(cv2.imread(name), cv2.COLOR_RGB2GRAY)
                   for name in sorted(glob(path_to_characters+"*/*"))]
-    imgsize = (1000, 300)
+    imgsize = (300, 300)
     img = np.full(imgsize, 255, np.uint8)
 
     #  print cuneiform characters onto white back
@@ -37,9 +37,26 @@ if __name__ == "__main__":
     zoom = 5
     img = cv2.resize(img, (0, 0), fx=zoom, fy=zoom)
 
+    kernel_size = 5
+    kernel = cv2.getStructuringElement(
+        cv2.MORPH_ELLIPSE, (kernel_size, kernel_size))
+
     # erode
-    kernel = np.ones((2, 2), np.uint8)
-    #  img = cv2.erode(img, kernel, iterations=3)
+    img = cv2.erode(img, kernel, iterations=3)
+    cv2.imwrite("./out/out2.jpg", img)
+
+    # dilate and paint little by little
+    iteration = 30
+    img = img/iteration
+    imgbuf = img
+    for i in range(iteration):
+        imgbuf = cv2.dilate(imgbuf, kernel, iterations=2)
+        #  M = np.float32([[1, 0, -kernel_size//2], [0, 1, -kernel_size//2]])
+        #  imgbuf = cv2.warpAffine(imgbuf, M,imgbuf.shape[::-1])
+        #  img = np.maximum(img, imgbuf)
+        img = cv2.addWeighted(img, 1, imgbuf, 1, 0)
+
+
 
     #  optional lise detection. doesn't work well.
     detectLines = False
