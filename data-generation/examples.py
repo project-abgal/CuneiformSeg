1cc6ccdb4c0a5c19c1b23e168f698cc4ddb2179b
woody <piequalsabout314159@gmail.com>
2019-03-10 15:27:11 -0400

    lighting added.

--- data-generation/examples.py
+++ data-generation/examples.py
@@ -4,6 +4,7 @@ import numpy as np
 
 path_to_characters = "../hccc/images/"
 
+epsilon = 0.0001
 
 def subtract_char(background, patch, x, y):
     cv2.subtract(background[x:x+patch.shape[0], y:y+patch.shape[1]],
@@ -55,6 +56,33 @@ if __name__ == "__main__":
         #  imgbuf = cv2.warpAffine(imgbuf, M,imgbuf.shape[::-1])
         #  img = np.maximum(img, imgbuf)
         img = cv2.addWeighted(img, 1, imgbuf, 1, 0)
+
+    img /= np.max(img)/255
+
+    # calculate nomal vectors to simulate reflection
+    sobelx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=5)
+    sobely = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=5)
+    carving_shallowness = 1000
+    gradz = np.full(imgsize, carving_shallowness, np.float32)  # constant value
+    direction_vector = np.array(
+        (sobelx, sobely, gradz))
+    direction_vector /= np.sqrt(np.sum(direction_vector**2, axis=0))+epsilon
+    direction_vector = direction_vector.transpose((1, 2, 0))
+    print(direction_vector.shape)
+    #  print(np.full(imgsize, carving_shallowness, np.uint8).shape)
+    cv2.imwrite("./out/out2.jpg", sobelx)
+
+    # assume light is coming from above 60 degrees
+    light_direction_vector = np.array((0, -1, np.sqrt(3)))
+    outimg = np.dot(direction_vector, light_direction_vector)
+    #  normalize to have 255 as max
+    outimg /= np.max(outimg)
+    outimg *= 255
+    print(np.max(outimg))
+    cv2.imwrite("./out/out2.jpg", outimg)
+
+    # decrease roughness of image
+    #  outimg = cv2.blur(outimg, ksize=(5, 5))
 
     #  optional lise detection. doesn't work well.
     detectLines = False
